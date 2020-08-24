06e0cfa8e16d9bc3ce4fe9252e06bf714a08dba9
ayush <ayush@localhost.localdomain>
2020-08-24 17:59:15 +0530

    url

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import TfidfVectorizer
 import urllib.request
 from inscriptis import get_text
+import re
 
 cat_dict={
     0:'Bulletins_news_magazine',
@@ -57,6 +58,17 @@ def predict():
     
     url = request.form['url_to_predict']
     print(url)
+    regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    validate_url = re.match(regex, url) is not None
+    if validate_url == False:
+        url = "http://"+url
     try:
         html = urllib.request.urlopen(url).read().decode('utf-8')
         text = get_text(html)
@@ -72,11 +84,11 @@ def predict():
         category_predicted = model[0].predict(cat_vect)
     #     output=cat_data.target_names([int(category_predicted)])
 
-        return render_template('index.html', prediction_text='Predicted Category for {}: {}'.format(url,cat_dict.get(int(category_predicted))))
+        return render_template('index.html', prediction_text='{}: {}'.format(url,cat_dict.get(int(category_predicted))))
     except:
-        error="Caused error"
-        return render_template('index.html', error=error)
+        error="Check url again"
+        return render_template('index.html', prediction_text=error)
         
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
